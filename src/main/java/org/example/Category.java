eee77dad1d0d528853d6fd992e5ff69fa9856c73
Toky <tokiniainarakotozanany@gmail.com>
2025-12-09 19:31:54 +0300

    correction of encapsulation in category.java

--- src/main/java/org/example/Category.java
+++ src/main/java/org/example/Category.java
@@ -2,11 +2,15 @@ package org.example;
 
 public class Category {
     public int id;
-    public String name;
+    private String name;
 
     public Category(int id,  String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public String getCategoryName() {
+        return name;
     }
     
     @Override
